dcde7431cf850c24644dc0e49aa9a66f035a31e4
Michał Nowicki <michalsminowicki@gmail.com>
2014-03-17 01:06:40 +0100

    Adding EKF implementation

--- NavigationDG/jni/Android.mk
+++ NavigationDG/jni/Android.mk
@@ -1,5 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
+# Extended Kalman Filter library
+include $(CLEAR_VARS)
+include ../OpenCV-2.4.8-android-sdk/sdk/native/jni/OpenCV.mk
+
+LOCAL_MODULE    := EKF
+LOCAL_SRC_FILES := EKF/EKF/EKF.cpp EKF/EKF_export.cpp 
+LOCAL_LDLIBS +=  -llog -ldl
+
+include $(BUILD_SHARED_LIBRARY)
+
 # Visual Odometry library
 include $(CLEAR_VARS)
 include ../OpenCV-2.4.8-android-sdk/sdk/native/jni/OpenCV.mk
