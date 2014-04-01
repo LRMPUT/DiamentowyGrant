491a94882c1ff5fe0a89a2376f465ed3c3080fe8
Michał Nowicki <michalsminowicki@gmail.com>
2014-04-02 00:01:27 +0200

    Changing g2o library to static

--- NavigationDG/jni/GraphOptimization/libg2o/Android.mk
+++ NavigationDG/jni/GraphOptimization/libg2o/Android.mk
@@ -2,141 +2,141 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_calibration_odom_laser
-LOCAL_SRC_FILES := libg2o_calibration_odom_laser.so
+LOCAL_SRC_FILES := libg2o_calibration_odom_laser.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_cli
-LOCAL_SRC_FILES := libg2o_cli.so
+LOCAL_SRC_FILES := libg2o_cli.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_core
-LOCAL_SRC_FILES := libg2o_core.so
+LOCAL_SRC_FILES := libg2o_core.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_csparse_extension
-LOCAL_SRC_FILES := libg2o_csparse_extension.so
+LOCAL_SRC_FILES := libg2o_csparse_extension.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_ext_csparse
-LOCAL_SRC_FILES := libg2o_ext_csparse.so
+LOCAL_SRC_FILES := libg2o_ext_csparse.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_interface
-LOCAL_SRC_FILES := libg2o_interface.so
+LOCAL_SRC_FILES := libg2o_interface.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_parser
-LOCAL_SRC_FILES := libg2o_parser.so
+LOCAL_SRC_FILES := libg2o_parser.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_simulator
-LOCAL_SRC_FILES := libg2o_simulator.so
+LOCAL_SRC_FILES := libg2o_simulator.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_csparse
-LOCAL_SRC_FILES := libg2o_solver_csparse.so
+LOCAL_SRC_FILES := libg2o_solver_csparse.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_dense
-LOCAL_SRC_FILES := libg2o_solver_dense.so
+LOCAL_SRC_FILES := libg2o_solver_dense.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_eigen
-LOCAL_SRC_FILES := libg2o_solver_eigen.so
+LOCAL_SRC_FILES := libg2o_solver_eigen.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_pcg
-LOCAL_SRC_FILES := libg2o_solver_pcg.so
+LOCAL_SRC_FILES := libg2o_solver_pcg.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_slam2d_linear
-LOCAL_SRC_FILES := libg2o_solver_slam2d_linear.so
+LOCAL_SRC_FILES := libg2o_solver_slam2d_linear.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_solver_structure_only
-LOCAL_SRC_FILES := libg2o_solver_structure_only.so
+LOCAL_SRC_FILES := libg2o_solver_structure_only.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_stuff
-LOCAL_SRC_FILES := libg2o_stuff.so
+LOCAL_SRC_FILES := libg2o_stuff.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_tutorial_slam2d
-LOCAL_SRC_FILES := libg2o_tutorial_slam2d.so
+LOCAL_SRC_FILES := libg2o_tutorial_slam2d.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_data
-LOCAL_SRC_FILES := libg2o_types_data.so
+LOCAL_SRC_FILES := libg2o_types_data.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_icp
-LOCAL_SRC_FILES := libg2o_types_icp.so
+LOCAL_SRC_FILES := libg2o_types_icp.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_sba
-LOCAL_SRC_FILES := libg2o_types_sba.so
+LOCAL_SRC_FILES := libg2o_types_sba.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_sclam2d
-LOCAL_SRC_FILES := libg2o_types_sclam2d.so
+LOCAL_SRC_FILES := libg2o_types_sclam2d.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_sim3
-LOCAL_SRC_FILES := libg2o_types_sim3.so
+LOCAL_SRC_FILES := libg2o_types_sim3.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_slam2d
-LOCAL_SRC_FILES := libg2o_types_slam2d.so
+LOCAL_SRC_FILES := libg2o_types_slam2d.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := g2o_types_slam3d
-LOCAL_SRC_FILES := libg2o_types_slam3d.so
+LOCAL_SRC_FILES := libg2o_types_slam3d.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../csparse
-include $(PREBUILT_SHARED_LIBRARY)
+include $(PREBUILT_STATIC_LIBRARY)
 
 
 
